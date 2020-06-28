df0914a04a9794ec3b8bf19112fa2cccc3b406e8
Mamh-Linux <bright.ma@transsion.com>
2020-06-28 20:25:12 +0800

    src: java: format java code style

--- src/main/java/org/jenkinsci/plugins/elasticaxisplugin/ElasticAxis.java
+++ src/main/java/org/jenkinsci/plugins/elasticaxisplugin/ElasticAxis.java
@@ -7,7 +7,6 @@ import hudson.matrix.AxisDescriptor;
 import hudson.matrix.LabelAxis;
 import hudson.matrix.MatrixBuild;
 import hudson.model.AutoCompletionCandidates;
-import hudson.model.Messages;
 import hudson.model.Computer;
 import hudson.model.Label;
 import hudson.model.Node;
@@ -30,75 +29,75 @@ import antlr.ANTLRException;
 
 public class ElasticAxis extends LabelAxis {
 
-	private String label;
-	private boolean ignoreOffline;
-        private boolean dontExpandLabels;
+    private String label;
+    private boolean ignoreOffline;
+    private boolean dontExpandLabels;
 
     @DataBoundConstructor
-    public ElasticAxis(String name, String labelString, 
-            boolean ignoreOffline, boolean dontExpandLabels) {
+    public ElasticAxis(String name, String labelString,
+                       boolean ignoreOffline, boolean dontExpandLabels) {
         super(name, computeAllNodesInLabel(labelString, dontExpandLabels));
-		this.label = labelString;
-		this.ignoreOffline = ignoreOffline;
-                this.dontExpandLabels = dontExpandLabels;
-    }
-    
-    public String getLabelString() {
-    	return label;
-    }
-    
-    public boolean getIgnoreOffline() {
-    	return ignoreOffline;
-    }
-    
-    public boolean getDontExpandLabels() {
-    	return dontExpandLabels;
+        this.label = labelString;
+        this.ignoreOffline = ignoreOffline;
+        this.dontExpandLabels = dontExpandLabels;
     }
 
-	@Override
+    public String getLabelString() {
+        return label;
+    }
+
+    public boolean getIgnoreOffline() {
+        return ignoreOffline;
+    }
+
+    public boolean getDontExpandLabels() {
+        return dontExpandLabels;
+    }
+
+    @Override
     public List<String> rebuild(MatrixBuild.MatrixBuildExecution context) {
         return computeNodesInLabel(label, ignoreOffline, dontExpandLabels);
     }
 
     @Override
     public List<String> getValues() {
-    	return computeAllNodesInLabel(label, dontExpandLabels);
+        return computeAllNodesInLabel(label, dontExpandLabels);
     }
 
     private static List<String> computeAllNodesInLabel(String labelName, Boolean dontExpandLabels) {
-		return computeNodesInLabel(labelName, false, dontExpandLabels);
-	}
-    
-	private static List<String> computeNodesInLabel(String labelWithNodes, boolean restrictToOnlineNodes, boolean dontExpandLabels) {
-		List<String> computedNodes=new ArrayList<String>();
-		String[] labels = labelWithNodes.split(",");
-		for (String aLabel : labels) {
-                    if(!dontExpandLabels){
-			for(Node node : Jenkins.getInstance().getLabel(aLabel.trim()).getNodes()) {
-				if (shouldAddNode(restrictToOnlineNodes, node.toComputer())) 
-					computedNodes.add(node.getSelfLabel().getExpression());
-			}
-                    } else {
-                        Boolean onlineNodesForLabel = false;
-                        for(Node node : Jenkins.getInstance().getLabel(aLabel.trim()).getNodes()) {
-				if (shouldAddNode(restrictToOnlineNodes, node.toComputer())) 
-					onlineNodesForLabel = true;
-			}
-                        if(onlineNodesForLabel)
-                            computedNodes.add(Jenkins.getInstance().getLabel(aLabel.trim()).getExpression());
-                    }
-		}
-		
-        return Collections.unmodifiableList(computedNodes);
-	}
+        return computeNodesInLabel(labelName, false, dontExpandLabels);
+    }
 
-	private static boolean shouldAddNode(boolean restrictToOnlineNodes, Computer c) {
-		if (c == null) return true;
-		if (!restrictToOnlineNodes) return true;
-		
-		boolean isNodeAvailable = (c.isOnline() || c.isConnecting()) && c.isAcceptingTasks();
-		return isNodeAvailable;
-	}
+    private static List<String> computeNodesInLabel(String labelWithNodes, boolean restrictToOnlineNodes, boolean dontExpandLabels) {
+        List<String> computedNodes = new ArrayList<String>();
+        String[] labels = labelWithNodes.split(",");
+        for (String aLabel : labels) {
+            if (!dontExpandLabels) {
+                for (Node node : Jenkins.getInstance().getLabel(aLabel.trim()).getNodes()) {
+                    if (shouldAddNode(restrictToOnlineNodes, node.toComputer()))
+                        computedNodes.add(node.getSelfLabel().getExpression());
+                }
+            } else {
+                Boolean onlineNodesForLabel = false;
+                for (Node node : Jenkins.getInstance().getLabel(aLabel.trim()).getNodes()) {
+                    if (shouldAddNode(restrictToOnlineNodes, node.toComputer()))
+                        onlineNodesForLabel = true;
+                }
+                if (onlineNodesForLabel)
+                    computedNodes.add(Jenkins.getInstance().getLabel(aLabel.trim()).getExpression());
+            }
+        }
+
+        return Collections.unmodifiableList(computedNodes);
+    }
+
+    private static boolean shouldAddNode(boolean restrictToOnlineNodes, Computer c) {
+        if (c == null) return true;
+        if (!restrictToOnlineNodes) return true;
+
+        boolean isNodeAvailable = (c.isOnline() || c.isConnecting()) && c.isAcceptingTasks();
+        return isNodeAvailable;
+    }
 
     @Extension
     public static class DescriptorImpl extends AxisDescriptor {
@@ -117,34 +116,34 @@ public class ElasticAxis extends LabelAxis {
                     formData.getBoolean("dontExpandLabels")
             );
         }
-        
+
         public FormValidation doCheckLabelString(@QueryParameter String value) {
-        	String[] labels = value.split(",");
-        	for (String oneLabel : labels) {
-        		FormValidation validation = checkOneLabel(oneLabel.trim());
-				if (!validation.equals(FormValidation.ok()))
-					return validation;
-			}
+            String[] labels = value.split(",");
+            for (String oneLabel : labels) {
+                FormValidation validation = checkOneLabel(oneLabel.trim());
+                if (!validation.equals(FormValidation.ok()))
+                    return validation;
+            }
             return FormValidation.ok();
         }
 
-		private FormValidation checkOneLabel(String oneLabel) {
-			if (Util.fixEmpty(oneLabel)==null)
-				return FormValidation.ok(); 
-			
-			Label l = Jenkins.getInstance().getLabel(oneLabel);
-			if (l.isEmpty()) {
-				for (LabelAtom a : l.listAtoms()) {
-					if (a.isEmpty()) {
-						LabelAtom nearest = LabelAtom.findNearest(a.getName());
-						return FormValidation.warning(Messages.AbstractProject_AssignedLabelString_NoMatch_DidYouMean(a.getName(),nearest.getDisplayName()));
-					}
-				}
-				return FormValidation.warning(Messages.AbstractProject_AssignedLabelString_NoMatch());
-			}
-			return FormValidation.ok();
-		}
-        
+        private FormValidation checkOneLabel(String oneLabel) {
+            if (Util.fixEmpty(oneLabel) == null)
+                return FormValidation.ok();
+
+            Label l = Jenkins.getInstance().getLabel(oneLabel);
+            if (l.isEmpty()) {
+                for (LabelAtom a : l.listAtoms()) {
+                    if (a.isEmpty()) {
+                        LabelAtom nearest = LabelAtom.findNearest(a.getName());
+                        return FormValidation.warning(Messages.AbstractProject_AssignedLabelString_NoMatch_DidYouMean(a.getName(), nearest.getDisplayName()));
+                    }
+                }
+                return FormValidation.warning(Messages.AbstractProject_AssignedLabelString_NoMatch());
+            }
+            return FormValidation.ok();
+        }
+
         public AutoCompletionCandidates doAutoCompleteLabelString(@QueryParameter String value) {
             AutoCompletionCandidates c = new AutoCompletionCandidates();
             Set<Label> labels = Jenkins.getInstance().getLabels();
